5fbb6bab25ea50f51ca26efdf4f1f4d3086161c8
joartspp <61728467+joartspp@users.noreply.github.com>
2021-06-10 09:36:54 +0700

    chore(progressbar) : add comment code

--- progressbar/app.js
+++ progressbar/app.js
@@ -1,36 +1,38 @@
+// Add percent function
 const addPercentage = (many) => {
-    var oldPencen = checkValue();
-    var percent = oldPencen + many;
-    if (percent >= 100) {
+    var oldPencen = checkValue(); //Receive now value from element
+    var percent = oldPencen + many; // New percent
+    if (percent >= 100) { // Condition when new percent more than 100. I fixed it
         percent = 100
     }
-    var newPencen = (628 - ((628 * percent)/100));
-    changeText(percent);
-    document.querySelector('#processor:nth-child(2)').style.strokeDashoffset = newPencen
+    var newPencen = (628 - ((628 * percent)/100)); // Formula for change percent to px
+    changeText(percent); //Change text
+    document.querySelector('#processor:nth-child(2)').style.strokeDashoffset = newPencen // Change percent to new percent
 }
 
-
+// Remove percent function
 const removePercentage = (many) => {
-    var oldPencen = checkValue();
-    var percent = oldPencen - many;
-    if (percent <= 0) {
+    var oldPencen = checkValue(); //Receive now value from element
+    var percent = oldPencen - many; // New percent
+    if (percent <= 0) { // Condition when new percent more than 0. I fixed it
         percent = 0
     }
-    var newPencen = (628 - ((628 * percent)/100));
-    changeText(percent);
-    document.querySelector('#processor:nth-child(2)').style.strokeDashoffset = newPencen
+    var newPencen = (628 - ((628 * percent)/100)); // Formula for change percent to px
+    changeText(percent); //Change text
+    document.querySelector('#processor:nth-child(2)').style.strokeDashoffset = newPencen // Change percent to new percent
 }
 
-
+// Check now percent function
 const checkValue = () => {
-    var el = document.querySelector('#processor:nth-child(2)');
-    var rawValue = getComputedStyle(el).strokeDashoffset;
-    var changeInt = parseInt(rawValue)
-    var calculate = Math.floor(((changeInt - 628) * 100)/(-628))
-    return (calculate);
+    var el = document.querySelector('#processor:nth-child(2)'); //Get element property
+    var rawValue = getComputedStyle(el).strokeDashoffset; // get all style computed from this element
+    var changeInt = parseInt(rawValue) // Convert PX to Int
+    var calculate = Math.floor(((changeInt - 628) * 100)/(-628)) // Formula for change px to percent
+    return (calculate); // Return percent
 };
 
+// Change text function
 const changeText = (percent) => {
-    var el = document.getElementById('num-prog');
-    el.innerText = `${percent}%`;
+    var el = document.getElementById('num-prog'); //get element
+    el.innerText = `${percent}%`;// set text in this element
 }
